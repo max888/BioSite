219be6ff54c6af95643f1c7be5e6cf226d4a61e4
Max Norton <maxnorton.nz@gmail.com>
2014-04-18 19:20:35 +0100

    bottom footer add link to code

--- js/application.js
+++ js/application.js
@@ -1,7 +1,63 @@
 $(document).ready(function() {
 
-  $('#about').waypoint(function() {
-    prompt('Basic example callback triggered.');
+  $('#about_link').on('click', function(e) {
+    e.preventDefault();
+    console.log("about working");
+    $.smoothScroll({
+      scrollElement: $('body'),
+      scrollTarget: '#about',
+      speed: 900,
+    });
+    return false;
   });
+
+  $('#home_link').on('click', function(e) {
+    e.preventDefault();
+    console.log("home working");
+    $.smoothScroll({
+      scrollElement: $('body'),
+      scrollTarget: '#home',
+      speed: 900,
+
+    });
+    return false;
+  });
+
+  $('#portfolio_link').on('click', function(e) {
+    e.preventDefault();
+    console.log("portfolio working");
+    $.smoothScroll({
+      scrollElement: $('body'),
+      scrollTarget: '#portfolio',
+      speed: 900,
+    });
+    return false;
+  });
+
+  $('#contact_link').on('click', function(e) {
+    e.preventDefault();
+    console.log("contact working");
+    $.smoothScroll({
+      scrollElement: $('body'),
+      scrollTarget: '#contact',
+      speed: 900,
+    });
+    return false;
+  });
+
+  // An alternative way to scroll to the links smoothly
+  
+  // $('a[href^="#"]').on('click',function (e) {
+  //     e.preventDefault();
+
+  //     var target = this.hash,
+  //     $target = $(target);
+
+  //     $('html, body').stop().animate({
+  //         'scrollTop': $target.offset().top
+  //     }, 900, 'swing', function () {
+  //         window.location.hash = target;
+  //     });
+  // });
 
 });
